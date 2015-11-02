0cb75c0f7083e21e7dd6d92ead962e4fbb78a882
Jesse <jcjessecai@gmail.com>
2015-11-02 00:21:28 -0800

    finishe

--- src/costFunction.cpp
+++ src/costFunction.cpp
@@ -1,39 +1,40 @@
 #include<armadillo>
 #include"costFunction.hpp"
 #include"printMat.hpp"
+#include"sigmoid.hpp"
 
 using namespace arma;
 
-double nnCostFunction(vec theta_large, int input_layer_size, int hidden_layer_size,int num_labels, mat X,umat y, double lambda, mat& gradient){
+double nnCostFunction(Col theta_large, int input_layer_size, int hidden_layer_size,int num_labels, mat X,umat y, double lambda, mat& gradient){
 	
 	//defining some variables 
-	int m = X.n_rows;
+	//int m = X.n_rows();
 	double J = 0;
 	
-	mat theta1 = mat(input_layer_size*hidden_layer_size, 1);
-	mat theta2 = mat(num_labels*hidden_layer_size, 1);
-	
+	//mat theta1 = mat(input_layer_size*hidden_layer_size, 1);
+	//mat theta2 = mat(num_labels*hidden_layer_size, 1);
+
 	//unrolls the parameters	
-	for(int i=0;i<theta_large.size();i++){
-		i < hidden_layer_size*input_layer_size? theta1(i,0) = theta_large(i) : theta2(i-(hidden_layer_size*input_layer_size),0) =theta_large(i);
-	}
-	theta1.resize(input_layer_size,hidden_layer_size);
-	theta2.resize(hidden_layer_size,num_labels);
+	//for(int i=0;i<theta_large.size();i++){
+	//	i < hidden_layer_size*input_layer_size? theta1(i,0) = theta_large(i) : theta2(i-(hidden_layer_size*input_layer_size),0) =theta_large(i);
+	//}
+	//theta1.resize(input_layer_size,hidden_layer_size);
+	//theta2.resize(hidden_layer_size,num_labels);
 	
 	//this implements the feedforward part of the neural network function
-	mat A1 = join_horiz(ones<mat>(m,1), X);	
+	/*mat A1 = join_horiz(ones<mat>(m,1), X);	
 	mat A2 = join_horiz(ones<mat>(m,1), sigmoid(A1*theta1.t()));	
-	h_theta = sigmoid(A2*theta2.t());
+	mat h_theta = sigmoid(A2*theta2.t());
 	
 	//computes the cost, parallelized for each y_i individually on each thread
 	#pragma omp parallel for
 	for (int i=0; i<num_labels; i++){
-		y_i = (y==i);
-		J += accu(-y_i%log(h_theta.col(i)) - (1-y_i)%log(1-h_theta.col(i))) / m 
-	}
+		mat y_i = conv_to<mat>::from(y==i);
+		J += accu(-y_i%log(h_theta.col(i)) - (1-y_i)%log(1-h_theta.col(i))) / m;
+	}*/
 	//adds regularization term to cost
-	J += (accu(square(theta1)) + accu(square(theta2)) - 2m) * (lambda/(2*m));
+	//J += (accu(square(theta1)) + accu(square(theta2)) - 2*m) * (lambda/(2*m));
 
 	//now it updates the gradient
-	return J	
+	return J;
 }
--- src/train.cpp
+++ src/train.cpp
@@ -2,9 +2,9 @@
 #include<armadillo>
 
 #include"randomInitialize.hpp"
-#include"sigmoid.hpp"
 #include"costFunction.hpp"
 #include"printMat.hpp"
+#include"sigmoid.hpp"
 
 using namespace arma;
 
@@ -12,10 +12,11 @@ int main(){
 	mat X = randu(3,20)*10;
 	umat y = conv_to<umat>::from(randu(10,1)*10);
 
-	vec initialTheta = join_vert(vectorise(generateRandomMatrix(250,50)) , vectorise(generateRandomMatrix(50,10)));
+	vec initialTheta = vec(join_vert(vectorise(generateRandomMatrix(250,50)) , vectorise(generateRandomMatrix(50,10))));
 
-	mat grad = zeros(3,3)	
-	cout << costFunction(initialTheta, 250,50,10,X,y,1, grad) << endl;
-	
+	mat grad = zeros(3,3);
+	printMat(grad, "grad");
+	double cost =  costFunction(initialTheta, 250,50,10,X,y,1, grad);
+	cout<<cost<<endl;	
 	return 0;
 }
